66888900f3ea14c837b4442b768cd43533d9f809
Slava Kim <slv@mit.edu>
2017-05-01 16:05:23 -0400

    client is multiline

--- src/client/gui.go
+++ src/client/gui.go
@@ -1,305 +1,231 @@
+// Copyright 2014 The gocui Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 package main
 
 import (
-	"github.com/mattn/go-runewidth"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
+	"strings"
+
+	"github.com/jroimartin/gocui"
 	"github.com/nsf/termbox-go"
-	"unicode/utf8"
 )
 
-func tbprint(x, y int, fg, bg termbox.Attribute, msg string) {
-	for _, c := range msg {
-		termbox.SetCell(x, y, c, fg, bg)
-		x += runewidth.RuneWidth(c)
+func nextView(g *gocui.Gui, v *gocui.View, key interface{}) error {
+	if v == nil || v.Name() == "side" {
+		_, err := g.SetCurrentView("main")
+		return err
 	}
+	_, err := g.SetCurrentView("side")
+	return err
 }
 
-func fill(x, y, w, h int, cell termbox.Cell) {
-	for ly := 0; ly < h; ly++ {
-		for lx := 0; lx < w; lx++ {
-			termbox.SetCell(x+lx, y+ly, cell.Ch, cell.Fg, cell.Bg)
+func cursorDown(g *gocui.Gui, v *gocui.View, key interface{}) error {
+	if v != nil {
+		cx, cy := v.Cursor()
+		if err := v.SetCursor(cx, cy+1); err != nil {
+			ox, oy := v.Origin()
+			if err := v.SetOrigin(ox, oy+1); err != nil {
+				return err
+			}
 		}
 	}
+	return nil
 }
 
-func rune_advance_len(r rune, pos int) int {
-	if r == '\t' {
-		return tabstop_length - pos%tabstop_length
+func cursorUp(g *gocui.Gui, v *gocui.View, key interface{}) error {
+	if v != nil {
+		ox, oy := v.Origin()
+		cx, cy := v.Cursor()
+		if err := v.SetCursor(cx, cy-1); err != nil && oy > 0 {
+			if err := v.SetOrigin(ox, oy-1); err != nil {
+				return err
+			}
+		}
 	}
-	return runewidth.RuneWidth(r)
+	return nil
 }
 
-func voffset_coffset(text []byte, boffset int) (voffset, coffset int) {
-	text = text[:boffset]
-	for len(text) > 0 {
-		r, size := utf8.DecodeRune(text)
-		text = text[size:]
-		coffset += 1
-		voffset += rune_advance_len(r, voffset)
+func getLine(g *gocui.Gui, v *gocui.View, key interface{}) error {
+	var l string
+	var err error
+
+	_, cy := v.Cursor()
+	if l, err = v.Line(cy); err != nil {
+		l = ""
 	}
-	return
-}
 
-func byte_slice_grow(s []byte, desired_cap int) []byte {
-	if cap(s) < desired_cap {
-		ns := make([]byte, len(s), desired_cap)
-		copy(ns, s)
-		return ns
+	maxX, maxY := g.Size()
+	if v, err := g.SetView("msg", maxX/2-30, maxY/2, maxX/2+30, maxY/2+2); err != nil {
+		if err != gocui.ErrUnknownView {
+			return err
+		}
+		fmt.Fprintln(v, l)
+		if _, err := g.SetCurrentView("msg"); err != nil {
+			return err
+		}
 	}
-	return s
+	return nil
 }
 
-func byte_slice_remove(text []byte, from, to int) []byte {
-	size := to - from
-	copy(text[from:], text[to:])
-	text = text[:len(text)-size]
-	return text
+func delMsg(g *gocui.Gui, v *gocui.View, key interface{}) error {
+	if err := g.DeleteView("msg"); err != nil {
+		return err
+	}
+	if _, err := g.SetCurrentView("side"); err != nil {
+		return err
+	}
+	return nil
 }
 
-func byte_slice_insert(text []byte, offset int, what []byte) []byte {
-	n := len(text) + len(what)
-	text = byte_slice_grow(text, n)
-	text = text[:n]
-	copy(text[offset+len(what):], text[offset:])
-	copy(text[offset:], what)
-	return text
+func quit(g *gocui.Gui, v *gocui.View, key interface{}) error {
+	return gocui.ErrQuit
 }
 
-const preferred_horizontal_threshold = 5
-const tabstop_length = 8
+func keybindings(g *gocui.Gui) error {
+	if err := g.SetKeybinding("side", gocui.KeyCtrlSpace, gocui.ModNone, nextView); err != nil {
+		return err
+	}
+	if err := g.SetKeybinding("main", gocui.KeyCtrlSpace, gocui.ModNone, nextView); err != nil {
+		return err
+	}
+	if err := g.SetKeybinding("side", gocui.KeyArrowDown, gocui.ModNone, cursorDown); err != nil {
+		return err
+	}
+	if err := g.SetKeybinding("side", gocui.KeyArrowUp, gocui.ModNone, cursorUp); err != nil {
+		return err
+	}
+	if err := g.SetKeybinding("", gocui.KeyCtrlC, gocui.ModNone, quit); err != nil {
+		return err
+	}
+	if err := g.SetKeybinding("side", gocui.KeyEnter, gocui.ModNone, getLine); err != nil {
+		return err
+	}
+	if err := g.SetKeybinding("msg", gocui.KeyEnter, gocui.ModNone, delMsg); err != nil {
+		return err
+	}
 
-type EditBox struct {
-	text           []byte
-	line_voffset   int
-	cursor_boffset int // cursor offset in bytes
-	cursor_voffset int // visual cursor offset in termbox cells
-	cursor_coffset int // cursor offset in unicode code points
+	if err := g.SetKeybinding("main", gocui.KeyCtrlS, gocui.ModNone, saveMain); err != nil {
+		return err
+	}
+	if err := g.SetKeybinding("main", gocui.KeyCtrlW, gocui.ModNone, saveVisualMain); err != nil {
+		return err
+	}
+	return nil
 }
 
-// Draws the EditBox in the given location, 'h' is not used at the moment
-func (eb *EditBox) Draw(x, y, w, h int) {
-	eb.AdjustVOffset(w)
+func saveMain(g *gocui.Gui, v *gocui.View, key interface{}) error {
+	f, err := ioutil.TempFile("", "gocui_demo_")
+	if err != nil {
+		return err
+	}
+	defer f.Close()
 
-	const coldef = termbox.ColorDefault
-	fill(x, y, w, h, termbox.Cell{Ch: ' '})
-
-	t := eb.text
-	lx := 0
-	tabstop := 0
+	p := make([]byte, 5)
+	v.Rewind()
 	for {
-		rx := lx - eb.line_voffset
-		if len(t) == 0 {
+		n, err := v.Read(p)
+		if n > 0 {
+			if _, err := f.Write(p[:n]); err != nil {
+				return err
+			}
+		}
+		if err == io.EOF {
 			break
 		}
-
-		if lx == tabstop {
-			tabstop += tabstop_length
-		}
-
-		if rx >= w {
-			termbox.SetCell(x+w-1, y, '→',
-				coldef, coldef)
-			break
-		}
-
-		r, size := utf8.DecodeRune(t)
-		if r == '\t' {
-			for ; lx < tabstop; lx++ {
-				rx = lx - eb.line_voffset
-				if rx >= w {
-					goto next
-				}
-
-				if rx >= 0 {
-					termbox.SetCell(x+rx, y, ' ', coldef, coldef)
-				}
-			}
-		} else {
-			if rx >= 0 {
-				termbox.SetCell(x+rx, y, r, coldef, coldef)
-			}
-			lx += runewidth.RuneWidth(r)
-		}
-	next:
-		t = t[size:]
-	}
-
-	if eb.line_voffset != 0 {
-		termbox.SetCell(x, y, '←', coldef, coldef)
-	}
-}
-
-// Adjusts line visual offset to a proper value depending on width
-func (eb *EditBox) AdjustVOffset(width int) {
-	ht := preferred_horizontal_threshold
-	max_h_threshold := (width - 1) / 2
-	if ht > max_h_threshold {
-		ht = max_h_threshold
-	}
-
-	threshold := width - 1
-	if eb.line_voffset != 0 {
-		threshold = width - ht
-	}
-	if eb.cursor_voffset-eb.line_voffset >= threshold {
-		eb.line_voffset = eb.cursor_voffset + (ht - width + 1)
-	}
-
-	if eb.line_voffset != 0 && eb.cursor_voffset-eb.line_voffset < ht {
-		eb.line_voffset = eb.cursor_voffset - ht
-		if eb.line_voffset < 0 {
-			eb.line_voffset = 0
+		if err != nil {
+			return err
 		}
 	}
+	return nil
 }
 
-func (eb *EditBox) MoveCursorTo(boffset int) {
-	eb.cursor_boffset = boffset
-	eb.cursor_voffset, eb.cursor_coffset = voffset_coffset(eb.text, boffset)
-}
-
-func (eb *EditBox) RuneUnderCursor() (rune, int) {
-	return utf8.DecodeRune(eb.text[eb.cursor_boffset:])
-}
-
-func (eb *EditBox) RuneBeforeCursor() (rune, int) {
-	return utf8.DecodeLastRune(eb.text[:eb.cursor_boffset])
-}
-
-func (eb *EditBox) MoveCursorOneRuneBackward() {
-	if eb.cursor_boffset == 0 {
-		return
+func saveVisualMain(g *gocui.Gui, v *gocui.View, key interface{}) error {
+	f, err := ioutil.TempFile("", "gocui_demo_")
+	if err != nil {
+		return err
 	}
-	_, size := eb.RuneBeforeCursor()
-	eb.MoveCursorTo(eb.cursor_boffset - size)
-}
+	defer f.Close()
 
-func (eb *EditBox) MoveCursorOneRuneForward() {
-	if eb.cursor_boffset == len(eb.text) {
-		return
+	vb := v.ViewBuffer()
+	if _, err := io.Copy(f, strings.NewReader(vb)); err != nil {
+		return err
 	}
-	_, size := eb.RuneUnderCursor()
-	eb.MoveCursorTo(eb.cursor_boffset + size)
+	return nil
 }
 
-func (eb *EditBox) MoveCursorToBeginningOfTheLine() {
-	eb.MoveCursorTo(0)
-}
-
-func (eb *EditBox) MoveCursorToEndOfTheLine() {
-	eb.MoveCursorTo(len(eb.text))
-}
-
-func (eb *EditBox) DeleteRuneBackward() {
-	if eb.cursor_boffset == 0 {
-		return
+func layout(g *gocui.Gui) error {
+	maxX, maxY := g.Size()
+	if v, err := g.SetView("side", -1, -1, 30, maxY); err != nil {
+		if err != gocui.ErrUnknownView {
+			return err
+		}
+		v.Highlight = true
+		v.SelBgColor = gocui.ColorGreen
+		v.SelFgColor = gocui.ColorBlack
+		fmt.Fprintln(v, "Item 1")
+		fmt.Fprintln(v, "Item 2")
+		fmt.Fprintln(v, "Item 3")
+		fmt.Fprint(v, "\rWill be")
+		fmt.Fprint(v, "deleted\rItem 4\nItem 5")
 	}
-
-	eb.MoveCursorOneRuneBackward()
-	_, size := eb.RuneUnderCursor()
-	eb.text = byte_slice_remove(eb.text, eb.cursor_boffset, eb.cursor_boffset+size)
-}
-
-func (eb *EditBox) DeleteRuneForward() {
-	if eb.cursor_boffset == len(eb.text) {
-		return
+	if v, err := g.SetView("main", 30, -1, maxX, maxY); err != nil {
+		if err != gocui.ErrUnknownView {
+			return err
+		}
+		fmt.Fprintf(v, "Hello, this is the initial text\nenjoy!\n")
+		v.Editable = true
+		v.Wrap = true
+		if _, err := g.SetCurrentView("main"); err != nil {
+			return err
+		}
 	}
-	_, size := eb.RuneUnderCursor()
-	eb.text = byte_slice_remove(eb.text, eb.cursor_boffset, eb.cursor_boffset+size)
-}
-
-func (eb *EditBox) DeleteTheRestOfTheLine() {
-	eb.text = eb.text[:eb.cursor_boffset]
-}
-
-func (eb *EditBox) InsertRune(r rune) {
-	var buf [utf8.UTFMax]byte
-	n := utf8.EncodeRune(buf[:], r)
-	eb.text = byte_slice_insert(eb.text, eb.cursor_boffset, buf[:n])
-	eb.MoveCursorOneRuneForward()
-}
-
-// Please, keep in mind that cursor depends on the value of line_voffset, which
-// is being set on Draw() call, so.. call this method after Draw() one.
-func (eb *EditBox) CursorX() int {
-	return eb.cursor_voffset - eb.line_voffset
-}
-
-var edit_box EditBox
-
-const edit_box_width = 30
-
-func redraw_all() {
-	const coldef = termbox.ColorDefault
-	termbox.Clear(coldef, coldef)
-	w, h := termbox.Size()
-
-	midy := h / 2
-	midx := (w - edit_box_width) / 2
-
-	// unicode box drawing chars around the edit box
-	termbox.SetCell(midx-1, midy, '│', coldef, coldef)
-	termbox.SetCell(midx+edit_box_width, midy, '│', coldef, coldef)
-	termbox.SetCell(midx-1, midy-1, '┌', coldef, coldef)
-	termbox.SetCell(midx-1, midy+1, '└', coldef, coldef)
-	termbox.SetCell(midx+edit_box_width, midy-1, '┐', coldef, coldef)
-	termbox.SetCell(midx+edit_box_width, midy+1, '┘', coldef, coldef)
-	fill(midx, midy-1, edit_box_width, 1, termbox.Cell{Ch: '─'})
-	fill(midx, midy+1, edit_box_width, 1, termbox.Cell{Ch: '─'})
-
-	edit_box.Draw(midx, midy, edit_box_width, 1)
-	termbox.SetCursor(midx+edit_box.CursorX(), midy)
-
-	tbprint(midx+6, midy+3, coldef, coldef, "Press ESC to quit")
-	termbox.Flush()
+	return nil
 }
 
 func start_ui(cl *OTClient) {
-	err := termbox.Init()
+	g, err := gocui.NewGui(gocui.OutputNormal)
 	if err != nil {
-		panic(err)
+		log.Panicln(err)
 	}
-	defer termbox.Close()
-	termbox.SetInputMode(termbox.InputEsc)
+	defer g.Close()
 
-	redraw_all()
-mainloop:
-	for {
-		switch ev := termbox.PollEvent(); ev.Type {
-		case termbox.EventKey:
-			pos := edit_box.cursor_voffset
-			switch ev.Key {
-			case termbox.KeyEsc:
-				break mainloop
-			case termbox.KeyArrowLeft, termbox.KeyCtrlB:
-				edit_box.MoveCursorOneRuneBackward()
-			case termbox.KeyArrowRight, termbox.KeyCtrlF:
-				edit_box.MoveCursorOneRuneForward()
-			case termbox.KeyBackspace, termbox.KeyBackspace2:
-				edit_box.DeleteRuneBackward()
-				cl.Delete(pos)
-			//case termbox.KeyDelete, termbox.KeyCtrlD:
-			//	edit_box.DeleteRuneForward()
-			case termbox.KeyTab:
-				edit_box.InsertRune('\t')
-				cl.Insert('\t', pos)
-			case termbox.KeySpace:
-				edit_box.InsertRune(' ')
-				cl.Insert(' ', pos)
-			//case termbox.KeyCtrlK:
-			//	edit_box.DeleteTheRestOfTheLine()
-			//case termbox.KeyHome, termbox.KeyCtrlA:
-			//	edit_box.MoveCursorToBeginningOfTheLine()
-			//case termbox.KeyEnd, termbox.KeyCtrlE:
-			//	edit_box.MoveCursorToEndOfTheLine()
-			default:
-				if ev.Ch != 0 {
-					edit_box.InsertRune(ev.Ch)
-					cl.Insert(ev.Ch, pos)
-				}
+	g.Cursor = true
+
+	g.SetManagerFunc(layout)
+
+	if err := keybindings(g); err != nil {
+		log.Panicln(err)
+	}
+
+	if err := g.SetEventHandler("main", func(g *gocui.Gui, v *gocui.View, e interface{}) error {
+		ev := e.(*termbox.Event)
+		cl.Insert('a', 0)
+		switch ev.Key {
+		case termbox.KeyEsc:
+		case termbox.KeyArrowLeft, termbox.KeyCtrlB:
+		case termbox.KeyArrowRight, termbox.KeyCtrlF:
+		case termbox.KeyBackspace, termbox.KeyBackspace2:
+			cl.Delete(0)
+		case termbox.KeyTab:
+			cl.Insert('\t', 0)
+		case termbox.KeySpace:
+			cl.Insert(' ', 0)
+		default:
+			if ev.Ch != 0 {
+				cl.Insert(ev.Ch, 0)
 			}
-		case termbox.EventError:
-			panic(ev.Err)
 		}
-		redraw_all()
+		return nil
+	}); err != nil {
+		log.Panicln(err)
+	}
+
+	if err := g.MainLoop(); err != nil && err != gocui.ErrQuit {
+		log.Panicln(err)
 	}
 }
